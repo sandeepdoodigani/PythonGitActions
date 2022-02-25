548dcf0fd2acf7d38a68fc6b67bc5de675b2b9e5
Gnaneshwar Bandari <31314778+gnaneshwarbandari@users.noreply.github.com>
2022-02-25 16:53:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-#IBM Watson IOT Platform
-#pip install wiotp-sdk
-import wiotp.sdk.device
 import time
 import random
+import wiotp.sdk.device
+
 myConfig = { 
     "identity": {
         "orgId": "hj5fmy",
@@ -16,7 +15,7 @@ myConfig = {
 
 def myCommandCallback(cmd):
     print("Message received from IBM IoT Platform: %s" % cmd.data['command'])
-    m=cmd.data['command']
+    #m=cmd.data['command']
 
 client = wiotp.sdk.device.DeviceClient(config=myConfig, logHandlers=None)
 client.connect()
